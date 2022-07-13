00b2e1a74a680aadeaba03ddc2a21e6909d2ac0a
Rishappreet Singh Moonga <63499257+Rishappreet-Singh@users.noreply.github.com>
2022-07-13 17:28:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,34 +11,33 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "resource" {
-  name     = "azure-functions-rg"
+  name     = "resources"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "demmetothemoon"
+  name                     = "linuxfunctionappsa"
   resource_group_name      = azurerm_resource_group.resource.name
   location                 = azurerm_resource_group.resource.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service_plan" "service_plan" {
-  name                = "azure-functions-service-plan"
-  location            = azurerm_resource_group.resource.location
+resource "azurerm_service_plan" "service-plan" {
+  name                = "app-service-plan"
   resource_group_name = azurerm_resource_group.resource.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  location            = azurerm_resource_group.resource.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
-resource "azurerm_function_app" "function_app" {
-  name                       = "azure-functions"
-  location                   = azurerm_resource_group.resource.location
-  resource_group_name        = azurerm_resource_group.resource.name
-  app_service_plan_id        = azurerm_app_service_plan.service_plan.id
-  storage_account_name       = azurerm_storage_account.storage.name
-  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+resource "azurerm_linux_function_app" "app" {
+  name                = "linux-function-app"
+  resource_group_name = azurerm_resource_group.resource.name
+  location            = azurerm_resource_group.resource.location
+
+  storage_account_name = azurerm_storage_account.storage.name
+  service_plan_id      = azurerm_service_plan.service-plan.id
+
+  site_config {}
 }
