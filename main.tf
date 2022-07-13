fe1a2077c061f3380342e6bf9ac00754c7dd009c
Rishappreet Singh Moonga <63499257+Rishappreet-Singh@users.noreply.github.com>
2022-07-13 18:21:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,31 +10,31 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  name     = "user_VMUQTRRGUZ_ResourceGroup"
+resource "azurerm_resource_group" "resource" {
+  name     = "resources"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "demoforthei"
-  resource_group_name      = local.name
-  location                 = local.location
+  name                     = "linuxfunctionappsa"
+  resource_group_name      = azurerm_resource_group.resource.name
+  location                 = azurerm_resource_group.resource.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_service_plan" "service-plan" {
   name                = "app-service-plan"
-  resource_group_name = local.name
-  location            = local.location
+  resource_group_name = azurerm_resource_group.resource.name
+  location            = azurerm_resource_group.resource.location
   os_type             = "Linux"
   sku_name            = "Y1"
 }
 
 resource "azurerm_linux_function_app" "app" {
   name                = "linux-function-app"
-  resource_group_name = local.name
-  location            = local.location
+  resource_group_name = azurerm_resource_group.resource.name
+  location            = azurerm_resource_group.resource.location
 
   storage_account_name = azurerm_storage_account.storage.name
   service_plan_id      = azurerm_service_plan.service-plan.id
