2444bbdd3f1bf84e6030cc691b07294ab3ecefcf
Rishappreet Singh Moonga <63499257+Rishappreet-Singh@users.noreply.github.com>
2022-07-14 14:51:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,30 @@ provider "azurerm" {
 }
 
 locals {
-  name     = "user_0LI23NQ99U_ResourceGroup"
+  name     = "user_24J35D37CA_ResourceGroup"
   location = "West Europe"
 }
 
+resource "azurerm_servicebus_namespace" "example" {
+  name                = "servicebus-namespace"
+  location            = local.location
+  resource_group_name = local.name
+  sku                 = "Standard"
+
+  tags = {
+    source = "terraform"
+  }
+}
+
+resource "azurerm_servicebus_queue" "example" {
+  name         = "servicebus-queue"
+  namespace_id = azurerm_servicebus_namespace.example.id
+
+  enable_partitioning = true
+}
+
 resource "azurerm_storage_account" "storage" {
-  name                     = "demmitothemoon"
+  name                     = "demmier"
   resource_group_name      = local.name
   location                 = local.location
   account_tier             = "Standard"
