29371807f9957092933186ac82aa8eb29cb497ae
Rishappreet Singh Moonga <63499257+Rishappreet-Singh@users.noreply.github.com>
2022-07-15 12:48:48 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+import azure.functions as func
+
+def main(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
+
+    input_msg = req.params.get('message')
+
+    msg.set(input_msg)
+
+    return 'OK'
