a07df05c1a50e0839f2ab13e0f10cbc2377b6c83
Rishappreet Singh Moonga <63499257+Rishappreet-Singh@users.noreply.github.com>
2022-07-15 17:37:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,48 +15,48 @@ locals {
   location = "West Europe"
 }
 
-# resource "azurerm_servicebus_namespace" "example" {
-#   name                = "servicebus-namespacerse"
-#   location            = local.location
-#   resource_group_name = local.name
-#   sku                 = "Standard"
+resource "azurerm_servicebus_namespace" "example" {
+  name                = "servicebus-namespacerse"
+  location            = local.location
+  resource_group_name = local.name
+  sku                 = "Standard"
 
-#   tags = {
-#     source = "terraform"
-#   }
-# }
+  tags = {
+    source = "terraform"
+  }
+}
 
-# resource "azurerm_servicebus_queue" "example" {
-#   name         = "servicebus-queue"
-#   namespace_id = azurerm_servicebus_namespace.example.id
+resource "azurerm_servicebus_queue" "example" {
+  name         = "servicebus-queue"
+  namespace_id = azurerm_servicebus_namespace.example.id
 
-#   enable_partitioning = true
-# }
+  enable_partitioning = true
+}
 
-# resource "azurerm_servicebus_namespace_authorization_rule" "example" {
-#   name         = "examplerule"
-#   namespace_id = azurerm_servicebus_namespace.example.id
+resource "azurerm_servicebus_namespace_authorization_rule" "example" {
+  name         = "examplerule"
+  namespace_id = azurerm_servicebus_namespace.example.id
 
-#   listen = true
-#   send   = true
-#   manage = false
-# }
+  listen = true
+  send   = true
+  manage = false
+}
 
-# resource "azurerm_storage_account" "storage" {
-#   name                     = "demmierthewa"
-#   resource_group_name      = local.name
-#   location                 = local.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+resource "azurerm_storage_account" "storage" {
+  name                     = "demmierthewa"
+  resource_group_name      = local.name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
-# resource "azurerm_service_plan" "service-plan" {
-#   name                = "app-service-plan-tt"
-#   resource_group_name = local.name
-#   location            = local.location
-#   os_type             = "Linux"
-#   sku_name            = "Y1"
-# }
+resource "azurerm_service_plan" "service-plan" {
+  name                = "app-service-plan-tt"
+  resource_group_name = local.name
+  location            = local.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
 
 resource "azurerm_linux_function_app" "app" {
   name                = "linux-function-app-for-azure"
